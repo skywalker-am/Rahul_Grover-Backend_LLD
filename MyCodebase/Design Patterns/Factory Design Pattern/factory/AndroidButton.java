c03297d6d462d78761a5cd4429c9873dba1d5658
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-13 19:09:45 +0530

    Update AndroidButton.java

--- MyCodebase/Design Patterns/Factory Design Pattern/factory/AndroidButton.java
+++ MyCodebase/Design Patterns/Factory Design Pattern/factory/AndroidButton.java
@@ -2,12 +2,12 @@ package factory;
 
 public class AndroidButton implements Button{
     @Override
-    public void setText() {
-
+    public void setText(String text) {
+        System.out.println(text);
     }
 
     @Override
     public void changeSize(int size) {
-
+         System.out.println(size);
     }
 }
