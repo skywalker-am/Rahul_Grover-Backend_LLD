50f1d4a141e886e18a8ae7e7f062af0cb53a9525
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-14 00:42:52 +0530

    sdfvfd

--- MyCodebase/Design Patterns/Abstract Factory Design Pattern/abstractFactory/factory/Button.java
+++
@@ -1,9 +0,0 @@
-package factory;
-
-public interface Button {
-
-    void setText();
-
-    void changeSize(int size);
-
-}
--- MyCodebase/Design Patterns/Abstract Factory Design Pattern/abstractFactory/factory/Dropdown.java
+++
@@ -1,8 +0,0 @@
-package factory;
-
-public interface Dropdown {
-
-    void addItem();
-
-    void click();
-}
