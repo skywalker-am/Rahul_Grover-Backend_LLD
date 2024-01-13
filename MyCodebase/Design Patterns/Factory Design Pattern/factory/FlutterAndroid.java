838d910a0641d25b1f510bf351ad0cd4db68f1ba
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-13 19:45:23 +0530

    Update FlutterAndroid.java

--- MyCodebase/Design Patterns/Factory Design Pattern/factory/FlutterAndroid.java
+++ MyCodebase/Design Patterns/Factory Design Pattern/factory/FlutterAndroid.java
@@ -5,4 +5,8 @@ public class FlutterAndroid extends Flutter{
   public Button createButton() {
     return new AndroidButton();
   }
+
+   public Dropdown createDropdown() {
+    return new AndroidDropdown();
+  }
 }
