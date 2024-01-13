200ac45130cdaff6c53aaf3e1931c500e452896c
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-13 19:34:52 +0530

    Update IosButton.java

--- MyCodebase/Design Patterns/Factory Design Pattern/factory/IosButton.java
+++ MyCodebase/Design Patterns/Factory Design Pattern/factory/IosButton.java
@@ -2,12 +2,12 @@ package factory;
 
 public class IosButton implements Button {
     @Override
-    public void setText() {
-
+    public void setText(String text) {
+        System.out.println(text);
     }
 
     @Override
     public void changeSize(int size) {
-
+         System.out.println(size);
     }
 }
