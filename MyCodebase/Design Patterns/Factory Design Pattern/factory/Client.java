34b762b3022f8c6288bc411bf2d4a1f66b1ac3bf
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-13 19:51:09 +0530

    Update Client.java

--- MyCodebase/Design Patterns/Factory Design Pattern/factory/Client.java
+++ MyCodebase/Design Patterns/Factory Design Pattern/factory/Client.java
@@ -4,9 +4,12 @@ public class Client {
     public static void main(String[] args) {
 
         Flutter flutter = new FlutterAndroid();
+        
         Button b = flutter.createButton();
         b.changeSize(21);
         b.setText("This is Android text update");
+
+        flutter.createDropdown();
 
         Flutter flutter = new FlutterIos();
         Button b = flutter.createButton();
