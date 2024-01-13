5e647670ab35c2b7a4f244c715b23ef3003d14d2
Abhishek Mishra <62464754+skywalker-am@users.noreply.github.com>
2024-01-13 19:17:03 +0530

    Update Client.java

--- MyCodebase/Design Patterns/Factory Design Pattern/factory/Client.java
+++ MyCodebase/Design Patterns/Factory Design Pattern/factory/Client.java
@@ -1,1 +1,14 @@
+package factory;
 
+public class Client {
+    public static void main(String[] args) {
+
+        Flutter flutter = new Flutter();
+
+        Button b = flutter.createButton("Android");
+
+        b.changeSize(21);
+        b.setText("This is text update");
+
+    }
+}
